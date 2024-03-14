fecf18fe2b1e068029f763d08685ee028b6b647b
Shubham Verma <115684266+Shubham6307@users.noreply.github.com>
2024-03-14 04:09:36 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Header from './components/Header/Header.jsx'
+import Home from './components/Home/Home.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import About from './components/About/About.jsx'
+
+import Footer from './components/Footer/Footer.jsx'
+import GitHub from './components/GitHub/GitHub.jsx'
+
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+   
+    children:[
+      {
+       path:"",
+       element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      },
+      {
+        // loader:{githubInfoLoader},
+        path:"GitHub",
+        element:<GitHub/>
+      }
+    ]
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <RouterProvider router={router}/>
+  
+  </React.StrictMode>,
+)
